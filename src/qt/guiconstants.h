c2bb55f2fef05129089bf48d12b190f44f2e4ca3
lizhi <cqtenq9@gmail.com>
2014-11-08 20:06:46 +0800

    update GUICONSTANTS

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -41,9 +41,9 @@ static const int MAX_PAYMENT_REQUEST_SIZE = 50000; // bytes
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 35
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
+#define QAPP_ORG_NAME "Feathercoin"
+#define QAPP_ORG_DOMAIN "feathercoin.com"
+#define QAPP_APP_NAME_DEFAULT "Feathercoin-Qt"
+#define QAPP_APP_NAME_TESTNET "Feathercoin-Qt-testnet"
 
 #endif // GUICONSTANTS_H
