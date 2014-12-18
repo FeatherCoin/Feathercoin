5abd3183c550fb9de271c10072f7e2db3cb25f47
lizhi <cqtenq9@gmail.com>
2014-12-18 17:06:24 +0800

    fix horizontalHeader
    modify  tableView->horizontalHeader()->resizeSection

--- src/qt/addressbookpage.cpp
+++ src/qt/addressbookpage.cpp
@@ -159,8 +159,8 @@ void AddressBookPage::setModel(AddressTableModel *model)
     ui->tableView->horizontalHeader()->setSectionResizeMode(AddressTableModel::Label, QHeaderView::Stretch);
     ui->tableView->horizontalHeader()->setSectionResizeMode(AddressTableModel::Address, QHeaderView::ResizeToContents);
 #endif
-		ui->tableView->horizontalHeader()->resizeSection(0, 140);
-		ui->tableView->horizontalHeader()->resizeSection(1, 600);
+		ui->tableView->horizontalHeader()->resizeSection(0, 150);
+		ui->tableView->horizontalHeader()->resizeSection(1, 900);
 
     connect(ui->tableView->selectionModel(), SIGNAL(selectionChanged(QItemSelection,QItemSelection)),
         this, SLOT(selectionChanged()));
