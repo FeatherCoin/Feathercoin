07525719698e379f2c721e8e6ffdc6a08468c843
Wellenreiter01 <wellenreiter01@t-online.de>
2015-11-09 18:44:16 +0100

    changed paperwallet template and copyright strings

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2011-2014 The Bitcoin developers
-// Copyright (c) 2013-2014 The Feathercoin developers
+// Copyright (c) 2011-2015 The Bitcoin developers
+// Copyright (c) 2013-2015 The Feathercoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
