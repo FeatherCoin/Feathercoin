a91664fc079079ddf03658fe8d85413fe64a37b0
13368272959@189.cn <ftcchina103629>
2016-01-01 21:49:15 +0800

    COPYRIGHT_YEAR, 2016

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -111,19 +111,19 @@ bool AppInit(int argc, char* argv[])
         // Command-line RPC
         bool fCommandLine = false;
         for (int i = 1; i < argc; i++)
-            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "bitcoin:"))
+            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "feathercoin:"))
                 fCommandLine = true;
 
         if (fCommandLine)
         {
-            fprintf(stderr, "Error: There is no RPC client functionality in bitcoind anymore. Use the bitcoin-cli utility instead.\n");
+            fprintf(stderr, "Error: There is no RPC client functionality in feathercoind anymore. Use the feathercoin-cli utility instead.\n");
             exit(1);
         }
 #ifndef WIN32
         fDaemon = GetBoolArg("-daemon", false);
         if (fDaemon)
         {
-            fprintf(stdout, "Bitcoin server starting\n");
+            fprintf(stdout, "Feathercoin server starting\n");
 
             // Daemonize
             pid_t pid = fork();
