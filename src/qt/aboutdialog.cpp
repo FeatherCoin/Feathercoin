33f23d1617b73462c2f5c97b286863b9a5850bd1
John Doering <ghostlander@gmail.com>
2016-03-09 20:43:13 +0200

    Feathercoin Qt Copyright Added

--- src/qt/aboutdialog.cpp
+++ src/qt/aboutdialog.cpp
@@ -15,7 +15,9 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->setupUi(this);
 
     // Set current copyright year
-    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The Bitcoin developers") + QString("<br>") + tr("Copyright") + QString(" &copy; ") + tr("2011-%1 The Litecoin developers").arg(ABOUTDIALOG_COPYRIGHT_YEAR));
+    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(ABOUTDIALOG_COPYRIGHT_YEAR) + tr("The Bitcoin developers") + QString("<br>") + \
+      tr("Copyright") + QString(" &copy; 2011-%1 ").arg(ABOUTDIALOG_COPYRIGHT_YEAR) + tr("The Litecoin developers") + QString("<br>") + \
+      tr("Copyright") + QString(" &copy; 2013-%1 ").arg(COPYRIGHT_YEAR) + tr("The Feathercoin developers"));
 }
 
 void AboutDialog::setModel(ClientModel *model)
