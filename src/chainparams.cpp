5fe871fe2a6d994326da6dfd2735a2068a64738d
wellenreiter01 <wellenreiter01@users.noreply.github.com>
2017-01-13 12:24:59 +0100

    Revert "Fixed boost c++11 build error - ambiguous overload in chainparams.cpp"

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -82,13 +82,13 @@ public:
 
 
         //how to do feathercoin ?
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,14); // FeatherCoin addresses start with F
-        base58Prefixes[SCRIPT_ADDRESS_OLD] = std::vector<unsigned char>(1,5);
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(14); // FeatherCoin addresses start with F
+        base58Prefixes[SCRIPT_ADDRESS_OLD] = list_of(5);
       //  base58Prefixes[SCRIPT_ADDRESS] = list_of(96);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,142);// 14+128
-        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xBC)(0x26).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xDA)(0xEE).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[SCRIPT_ADDRESS] = list_of(5);
+        base58Prefixes[SECRET_KEY] =     list_of(142);// 14+128
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xBC)(0x26);
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xDA)(0xEE);
         //base58Prefixes[EXT_SCRIPT_ADDRESS] = list_of(96);
         
         // Convert the pnSeeds array into usable address objects.
@@ -154,11 +154,11 @@ public:
 
         // Boost sucks, and should not be used. Workaround for Boost not being compatible with C++11;
         
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,65);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
-        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1,193);//65+128
-        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(65);
+        base58Prefixes[SCRIPT_ADDRESS] = list_of(196);
+        base58Prefixes[SECRET_KEY]     = list_of(193);//65+128
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x35)(0x87)(0xCF);
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94);
     }
     virtual const CBlock& GenesisBlock() const { return genesis; }
     virtual Network NetworkID() const { return CChainParams::TESTNET; }
