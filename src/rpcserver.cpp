ff028cb85cb395b6e7f82c0bace7ec9f9ae36cd4
aciddude <david@Walton.io>
2017-02-07 20:53:04 +0000

    Added "sendalert" command to rpc server

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -237,6 +237,7 @@ static const CRPCCommand vRPCCommands[] =
     { "getnettotals",           &getnettotals,           true,      true,       false },
     { "getpeerinfo",            &getpeerinfo,            true,      false,      false },
     { "ping",                   &ping,                   true,      false,      false },
+	{ "sendalert",     			&sendalert,      		 true,      false,      true  },
 
     /* Block chain and UTXO */
     { "getblockchaininfo",      &getblockchaininfo,      true,      false,      false },
