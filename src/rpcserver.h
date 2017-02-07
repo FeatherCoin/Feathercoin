882f0721e239f9a988138c140ef8caf041fd830e
aciddude <david@Walton.io>
2017-02-07 20:53:04 +0000

    Added "sendalert" command to rpc server cherry-pick ff028cb85cb395b6e7f82c0bace7ec9f9ae36cd4 from 0.9.6.1 solves issue #195

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -154,6 +154,8 @@ extern json_spirit::Value getaddednodeinfo(const json_spirit::Array& params, boo
 extern json_spirit::Value getnettotals(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value makekeypair(const json_spirit::Array& params, bool fHelp);
 
+extern json_spirit::Value sendalert(const json_spirit::Array& params, bool fHelp);
+
 extern json_spirit::Value getcheckpoint(const json_spirit::Array& params, bool fHelp); // in checkpointsync.cpp
 extern json_spirit::Value sendcheckpoint(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value enforcecheckpoint(const json_spirit::Array& params, bool fHelp);
