bc06984f33c02573877ae2a4339fa23ea18ccb5e
wrapperband <drajdoyle@aol.com>
2017-08-20 15:49:43 +0100

    Update nDefaultDbCache = from 100 to 500 and  nMinDbCache = 4 to 10   Reveiw FTC sync speed / LTC change

--- src/txdb.h
+++ src/txdb.h
@@ -20,11 +20,11 @@ struct CDiskTxPos;
 class uint256;
 
 //! -dbcache default (MiB)
-static const int64_t nDefaultDbCache = 100;
+static const int64_t nDefaultDbCache = 500;
 //! max. -dbcache in (MiB)
 static const int64_t nMaxDbCache = sizeof(void*) > 4 ? 16384 : 1024;
 //! min. -dbcache in (MiB)
-static const int64_t nMinDbCache = 4;
+static const int64_t nMinDbCache = 10;
 
 /** CCoinsView backed by the LevelDB coin database (chainstate/) */
 class CCoinsViewDB : public CCoinsView
