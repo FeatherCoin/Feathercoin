018150a325146cd32d847fbf1259805ff96e1588
wrapperband <drajdoyle@aol.com>
2017-08-21 10:53:47 +0100

    0.11.2 debug message review & sync speed test results : noui.cpp

--- src/noui.cpp
+++ src/noui.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin Core developers
+// Copyright (c) 2015-2017 The Feathercoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -34,14 +35,16 @@ static bool noui_ThreadSafeMessageBox(const std::string& message, const std::str
     }
 
     if (!fSecure)
-        LogPrintf("%s: %s\n", strCaption, message);
+    {
+        // if (fDebug) LogPrintf("%s: %s\n", strCaption, message);
+    }
     fprintf(stderr, "%s: %s\n", strCaption.c_str(), message.c_str());
     return false;
 }
 
 static void noui_InitMessage(const std::string& message)
 {
-    LogPrintf("init message: %s\n", message);
+    // if (fDebug) LogPrintf("init message: %s\n", message);
 }
 
 void noui_connect()
