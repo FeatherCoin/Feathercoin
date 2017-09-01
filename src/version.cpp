601e48345085d7fe24bd91d438a3aa29d9249ed0
Peter Bushnell <bushsolo@gmail.com>
2017-09-01 11:35:52 +0100

    If found git will define GIT_ARCHIVE

--- src/version.cpp
+++ src/version.cpp
@@ -35,7 +35,6 @@ const std::string CLIENT_NAME("Feathercoin");
 #endif
 
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID "$Format:%h$"
 #    define GIT_COMMIT_DATE "$Format:%cD$"
