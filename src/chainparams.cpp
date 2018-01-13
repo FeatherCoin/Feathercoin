109880180383f491131b002ca5a757047506937f
Peter Bushnell <bushsolo@gmail.com>
2018-01-13 13:19:29 +0000

    Hard fork three

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -103,6 +103,7 @@ public:
 
         consensus.nForkOne = 33000;
         consensus.nForkTwo = 87948;
+        consensus.nForkThree = 204639;
 
         genesis = CreateGenesisBlock(1317972665, 2084524493, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
@@ -197,6 +198,7 @@ public:
         nPruneAfterHeight = 1000;
 
         consensus.nForkTwo = 0;
+        consensus.nForkThree = 0;
 
         genesis = CreateGenesisBlock(1396255061, 3250989159, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
@@ -276,6 +278,7 @@ public:
         nPruneAfterHeight = 1000;
 
         consensus.nForkTwo = 0;
+        consensus.nForkThree = 0;
 
         genesis = CreateGenesisBlock(1515840634, 0, 0x207fffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
--- src/consensus/params.h
+++ src/consensus/params.h
@@ -62,6 +62,7 @@ struct Params {
     int64_t nPowTargetTimespan;
     int nForkOne;
     int nForkTwo;
+    int nForkThree;
     int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
     uint256 nMinimumChainWork;
 };
