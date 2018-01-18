1c15b89e71fff7d396f0b253cc2b80fef97b1d14
Peter Bushnell <bushsolo@gmail.com>
2018-01-18 15:12:01 +0000

    Payment request rename

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -80,7 +80,7 @@ namespace // Anon namespace
 //
 static QString ipcServerName()
 {
-    QString name("BitcoinQt");
+    QString name("FeathercoinQt");
 
     // Append a simple hash of the datadir
     // Note that GetDataDir(true) returns a different path
@@ -451,7 +451,7 @@ void PaymentServer::handleURIOrFile(const QString& s)
             }
             else
                 Q_EMIT message(tr("URI handling"),
-                    tr("URI cannot be parsed! This can be caused by an invalid Bitcoin address or malformed URI parameters."),
+                    tr("URI cannot be parsed! This can be caused by an invalid Feathercoin address or malformed URI parameters."),
                     CClientUIInterface::ICON_WARNING);
 
             return;
