cea6e0445b45ec147c3fa7bb77b87fbdbd659990
Peter Bushnell <bushsolo@gmail.com>
2018-03-14 11:41:23 +0000

    Remove suffix

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -48,7 +48,7 @@ const std::string CLIENT_NAME("Feathercoin");
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-beta"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build)
 
 #define BUILD_DESC_FROM_COMMIT(maj, min, rev, build, commit) \
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
