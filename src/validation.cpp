4f6d5543713dcbe3d0fafa32500f2eb53bda302b
Peter Bushnell <bushsolo@gmail.com>
2018-05-13 16:00:48 +0100

    Regtest block subsidy

--- src/validation.cpp
+++ src/validation.cpp
@@ -1135,6 +1135,10 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex, const Consensus
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
     int halvings = (nHeight + 306960) / consensusParams.nSubsidyHalvingInterval;
+
+    if (Params().NetworkIDString() == CBaseChainParams::REGTEST)
+        halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
+
     // Force block reward to zero when right shift is undefined.
     if (halvings >= 64)
         return 0;
