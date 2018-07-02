c3044f262edb4dc6be803aeabdbbfa70ff797d38
fanquake <fanquake@gmail.com>
2018-07-02 10:26:47 +0800

    net: Remove unused interrupt from SendMessages

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3286,7 +3286,7 @@ public:
     }
 };
 
-bool PeerLogicValidation::SendMessages(CNode* pto, std::atomic<bool>& interruptMsgProc)
+bool PeerLogicValidation::SendMessages(CNode* pto)
 {
     const Consensus::Params& consensusParams = Params().GetConsensus();
     {
