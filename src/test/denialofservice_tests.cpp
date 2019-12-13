03abc4c792724e3da6b43c23b0b0eb28d2195f0a
Peter Bushnell <bushsolo@gmail.com>
2019-12-13 13:58:14 +0000

    Update nPowTargetSpacing in DoS tests

--- src/test/denialofservice_tests.cpp
+++ src/test/denialofservice_tests.cpp
@@ -170,7 +170,7 @@ BOOST_AUTO_TEST_CASE(stale_tip_peer_management)
         BOOST_CHECK(node->fDisconnect == false);
     }
 
-    SetMockTime(GetTime() + 4*consensusParams.nPowTargetSpacing + 1);
+    SetMockTime(GetTime() + 12*consensusParams.nPowTargetSpacing + 1);
 
     // Now tip should definitely be stale, and we should look for an extra
     // outbound peer
