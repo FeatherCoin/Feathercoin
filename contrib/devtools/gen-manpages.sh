ec6580e80696f7fd3c684f213c1a69a8222375d5
Peter Bushnell <bushsolo@gmail.com>
2020-06-19 13:43:21 +0100

    Generate help2man docs

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,11 +7,11 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/bitcoind}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/bitcoin-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/bitcoin-tx}
-WALLET_TOOL=${WALLET_TOOL:-$BINDIR/bitcoin-wallet}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/bitcoin-qt}
+BITCOIND=${BITCOIND:-$BINDIR/feathercoind}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/feathercoin-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/feathercoin-tx}
+WALLET_TOOL=${WALLET_TOOL:-$BINDIR/feathercoin-wallet}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/feathercoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
