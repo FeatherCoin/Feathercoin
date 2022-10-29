46bb3c74711276b911edf0f57876f17368e2e835
Wellenreiter <wellenreiter01@t-online.de>
2022-10-29 12:24:58 +0200

    fix compile problem with Qt5

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -2,15 +2,15 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <interfaces/node.h>
-#include <qt/trafficgraphwidget.h>
-#include <qt/clientmodel.h>
 
 #include <QPainter>
 #include <QColor>
 #include <QTimer>
 #include <QPainterPath>
 #include <cmath>
+#include <interfaces/node.h>
+#include <qt/trafficgraphwidget.h>
+#include <qt/clientmodel.h>
 
 #define DESIRED_SAMPLES         800
 
