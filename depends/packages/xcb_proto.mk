5f53554c1abe88a954584f0e7ca10e4bb1bd2a10
Bushstar <bushsolo@gmail.com>
2026-04-03 10:08:57 +0100

    depends: update xcb-proto to 1.17.0

--- depends/packages/xcb_proto.mk
+++ depends/packages/xcb_proto.mk
@@ -1,8 +1,8 @@
 package=xcb_proto
-$(package)_version=1.10
-$(package)_download_path=https://xcb.freedesktop.org/dist
-$(package)_file_name=xcb-proto-$($(package)_version).tar.bz2
-$(package)_sha256_hash=7ef40ddd855b750bc597d2a435da21e55e502a0fefa85b274f2c922800baaf05
+$(package)_version=1.17.0
+$(package)_download_path=https://xorg.freedesktop.org/archive/individual/proto
+$(package)_file_name=xcb-proto-$($(package)_version).tar.gz
+$(package)_sha256_hash=392d3c9690f8c8202a68fdb89c16fd55159ab8d65000a6da213f4a1576e97a16
 
 define $(package)_config_cmds
   $($(package)_autoconf)
@@ -18,5 +18,6 @@ endef
 
 define $(package)_postprocess_cmds
   find -name "*.pyc" -delete && \
-  find -name "*.pyo" -delete
+  find -name "*.pyo" -delete && \
+  rm -rf lib/python*/site-packages/xcbgen/__pycache__
 endef
