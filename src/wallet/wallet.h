73f20432bfb5eb56deace76e62cb3e05384be7a5
Bushstar <bushsolo@gmail.com>
2026-04-04 11:31:35 +0100

    wallet: align high-fee warning thresholds with Feathercoin defaults

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -92,9 +92,9 @@ static const bool DEFAULT_DISABLE_WALLET = false;
 //! -maxtxfee default
 constexpr CAmount DEFAULT_TRANSACTION_MAXFEE{20 * COIN};
 //! Discourage users to set fees higher than this amount (in satoshis) per kB
-constexpr CAmount HIGH_TX_FEE_PER_KB{COIN / 100};
+constexpr CAmount HIGH_TX_FEE_PER_KB{5 * DEFAULT_FALLBACK_FEE};
 //! -maxtxfee will warn if called with a higher fee than this amount (in satoshis)
-constexpr CAmount HIGH_MAX_TX_FEE{100 * HIGH_TX_FEE_PER_KB};
+constexpr CAmount HIGH_MAX_TX_FEE{DEFAULT_TRANSACTION_MAXFEE};
 
 //! Pre-calculated constants for input size estimation in *virtual size*
 static constexpr size_t DUMMY_NESTED_P2WPKH_INPUT_SIZE = 91;
