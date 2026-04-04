d1bdab3c806fe43a0e03fddce1010c66053d87b2
Bushstar <bushsolo@gmail.com>
2026-04-04 15:39:40 +0100

    wallet,node: restore Feathercoin runtime message strings

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -631,7 +631,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(nullptr, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "bitcoin";
+    const char* pszModule = "feathercoin";
 #endif
     if (pex)
         return strprintf(
