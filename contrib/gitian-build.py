aff6e164c2563e8590134583d1a100126e33d9a1
Bushstar <bushsolo@gmail.com>
2026-04-12 13:43:17 +0100

    release: switch gitian flow to local unsigned attestations

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -8,14 +8,11 @@ import os
 import subprocess
 import sys
 
-PROJECT_NAME = 'feathercoin'
 SOURCE_REPO_DIR = 'feathercoin'
 BINARIES_DIR = 'feathercoin-binaries'
-GITIAN_SIGS_DIR = 'gitian.sigs'
-DETACHED_SIGS_DIR = 'feathercoin-detached-sigs'
+ATTESTATIONS_DIR = 'attestations'
 DEFAULT_SOURCE_URL = 'https://github.com/FeatherCoin/Feathercoin.git'
-DEFAULT_GITIAN_SIGS_URL = 'https://github.com/FeatherCoin/gitian.sigs.git'
-DEFAULT_DETACHED_SIGS_URL = 'https://github.com/FeatherCoin/feathercoin-detached-sigs.git'
+
 
 def setup():
     global args, workdir
@@ -34,14 +31,11 @@ def setup():
     else:
         programs += ['apt-cacher-ng', 'lxc', 'debootstrap']
     subprocess.check_call(['sudo', 'apt-get', 'install', '-qq'] + programs)
-    if not os.path.isdir(GITIAN_SIGS_DIR):
-        subprocess.check_call(['git', 'clone', args.gitian_sigs_url, GITIAN_SIGS_DIR])
-    if not os.path.isdir(DETACHED_SIGS_DIR):
-        subprocess.check_call(['git', 'clone', args.detached_sigs_url, DETACHED_SIGS_DIR])
     if not os.path.isdir('gitian-builder'):
         subprocess.check_call(['git', 'clone', 'https://github.com/devrandom/gitian-builder.git'])
     if not os.path.isdir(SOURCE_REPO_DIR):
         subprocess.check_call(['git', 'clone', args.url, SOURCE_REPO_DIR])
+    os.makedirs(args.attestations_dir, exist_ok=True)
     os.chdir('gitian-builder')
     make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
     if args.docker:
@@ -55,10 +49,12 @@ def setup():
         print('Reboot is required')
         sys.exit(0)
 
+
 def build():
     global args, workdir
 
     os.makedirs(BINARIES_DIR + '/' + args.version, exist_ok=True)
+    os.makedirs(args.attestations_dir, exist_ok=True)
     print('\nBuilding Dependencies\n')
     os.chdir('gitian-builder')
     os.makedirs('inputs', exist_ok=True)
@@ -69,118 +65,111 @@ def build():
 
     if args.linux:
         print('\nCompiling ' + args.version + ' Linux')
-        subprocess.check_call(['bin/gbuild', '-j', args.jobs, '-m', args.memory, '--commit', 'bitcoin='+args.commit, '--url', 'bitcoin='+args.url, '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-linux.yml'])
-        subprocess.check_call(['bin/gsign', '-p', args.sign_prog, '--signer', args.signer, '--release', args.version+'-linux', '--destination', '../' + GITIAN_SIGS_DIR + '/', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-linux.yml'])
+        subprocess.check_call([
+            'bin/gbuild', '-j', args.jobs, '-m', args.memory,
+            '--commit', 'bitcoin=' + args.commit,
+            '--url', 'bitcoin=' + args.url,
+            '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-linux.yml',
+        ])
+        subprocess.check_call([
+            'bin/gsign', '-p', args.sign_prog, '--signer', args.signer,
+            '--release', args.version + '-linux',
+            '--destination', '../' + args.attestations_dir + '/',
+            '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-linux.yml',
+        ])
         subprocess.check_call('mv build/out/feathercoin-*.tar.gz build/out/src/feathercoin-*.tar.gz ../' + BINARIES_DIR + '/' + args.version, shell=True)
 
     if args.windows:
         print('\nCompiling ' + args.version + ' Windows')
-        subprocess.check_call(['bin/gbuild', '-j', args.jobs, '-m', args.memory, '--commit', 'bitcoin='+args.commit, '--url', 'bitcoin='+args.url, '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win.yml'])
-        subprocess.check_call(['bin/gsign', '-p', args.sign_prog, '--signer', args.signer, '--release', args.version+'-win-unsigned', '--destination', '../' + GITIAN_SIGS_DIR + '/', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win.yml'])
+        subprocess.check_call([
+            'bin/gbuild', '-j', args.jobs, '-m', args.memory,
+            '--commit', 'bitcoin=' + args.commit,
+            '--url', 'bitcoin=' + args.url,
+            '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win.yml',
+        ])
+        subprocess.check_call([
+            'bin/gsign', '-p', args.sign_prog, '--signer', args.signer,
+            '--release', args.version + '-win-unsigned',
+            '--destination', '../' + args.attestations_dir + '/',
+            '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win.yml',
+        ])
         subprocess.check_call('mv build/out/feathercoin-*-win-unsigned.tar.gz inputs/', shell=True)
         subprocess.check_call('mv build/out/feathercoin-*.zip build/out/feathercoin-*.exe build/out/src/feathercoin-*.tar.gz ../' + BINARIES_DIR + '/' + args.version, shell=True)
 
     if args.macos:
         print('\nCompiling ' + args.version + ' MacOS')
-        subprocess.check_call(['bin/gbuild', '-j', args.jobs, '-m', args.memory, '--commit', 'bitcoin='+args.commit, '--url', 'bitcoin='+args.url, '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx.yml'])
-        subprocess.check_call(['bin/gsign', '-p', args.sign_prog, '--signer', args.signer, '--release', args.version+'-osx-unsigned', '--destination', '../' + GITIAN_SIGS_DIR + '/', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx.yml'])
+        subprocess.check_call([
+            'bin/gbuild', '-j', args.jobs, '-m', args.memory,
+            '--commit', 'bitcoin=' + args.commit,
+            '--url', 'bitcoin=' + args.url,
+            '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx.yml',
+        ])
+        subprocess.check_call([
+            'bin/gsign', '-p', args.sign_prog, '--signer', args.signer,
+            '--release', args.version + '-osx-unsigned',
+            '--destination', '../' + args.attestations_dir + '/',
+            '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx.yml',
+        ])
         subprocess.check_call('mv build/out/feathercoin-*-osx-unsigned.tar.gz inputs/', shell=True)
         subprocess.check_call('mv build/out/feathercoin-*.tar.gz build/out/feathercoin-*.dmg build/out/src/feathercoin-*.tar.gz ../' + BINARIES_DIR + '/' + args.version, shell=True)
 
     os.chdir(workdir)
 
-    if args.commit_files:
-        print('\nCommitting '+args.version+' Unsigned Sigs\n')
-        os.chdir(GITIAN_SIGS_DIR)
-        subprocess.check_call(['git', 'add', args.version+'-linux/'+args.signer])
-        subprocess.check_call(['git', 'add', args.version+'-win-unsigned/'+args.signer])
-        subprocess.check_call(['git', 'add', args.version+'-osx-unsigned/'+args.signer])
-        subprocess.check_call(['git', 'commit', '-m', 'Add '+args.version+' unsigned sigs for '+args.signer])
-        os.chdir(workdir)
-
-def sign():
-    global args, workdir
-    os.chdir('gitian-builder')
-
-    if args.windows:
-        print('\nSigning ' + args.version + ' Windows')
-        subprocess.check_call('cp inputs/feathercoin-' + args.version + '-win-unsigned.tar.gz inputs/bitcoin-win-unsigned.tar.gz', shell=True)
-        subprocess.check_call(['bin/gbuild', '--skip-image', '--upgrade', '--commit', 'signature='+args.commit, '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win-signer.yml'])
-        subprocess.check_call(['bin/gsign', '-p', args.sign_prog, '--signer', args.signer, '--release', args.version+'-win-signed', '--destination', '../' + GITIAN_SIGS_DIR + '/', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win-signer.yml'])
-        subprocess.check_call('mv build/out/bitcoin-*win64-setup.exe ../' + BINARIES_DIR + '/' + args.version + '/feathercoin-' + args.version + '-win64-setup.exe', shell=True)
-
-    if args.macos:
-        print('\nSigning ' + args.version + ' MacOS')
-        subprocess.check_call('cp inputs/feathercoin-' + args.version + '-osx-unsigned.tar.gz inputs/bitcoin-osx-unsigned.tar.gz', shell=True)
-        subprocess.check_call(['bin/gbuild', '--skip-image', '--upgrade', '--commit', 'signature='+args.commit, '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx-signer.yml'])
-        subprocess.check_call(['bin/gsign', '-p', args.sign_prog, '--signer', args.signer, '--release', args.version+'-osx-signed', '--destination', '../' + GITIAN_SIGS_DIR + '/', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx-signer.yml'])
-        subprocess.check_call('mv build/out/bitcoin-osx-signed.dmg ../' + BINARIES_DIR + '/' + args.version + '/feathercoin-' + args.version + '-osx.dmg', shell=True)
-
-    os.chdir(workdir)
-
-    if args.commit_files:
-        print('\nCommitting '+args.version+' Signed Sigs\n')
-        os.chdir(GITIAN_SIGS_DIR)
-        subprocess.check_call(['git', 'add', args.version+'-win-signed/'+args.signer])
-        subprocess.check_call(['git', 'add', args.version+'-osx-signed/'+args.signer])
-        subprocess.check_call(['git', 'commit', '-a', '-m', 'Add '+args.version+' signed binary sigs for '+args.signer])
-        os.chdir(workdir)
 
 def verify():
     global args, workdir
     rc = 0
     os.chdir('gitian-builder')
 
-    print('\nVerifying v'+args.version+' Linux\n')
-    if subprocess.call(['bin/gverify', '-v', '-d', '../' + GITIAN_SIGS_DIR + '/', '-r', args.version+'-linux', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-linux.yml']):
-        print('Verifying v'+args.version+' Linux FAILED\n')
+    print('\nVerifying v' + args.version + ' Linux\n')
+    if subprocess.call([
+        'bin/gverify', '-v', '-d', '../' + args.attestations_dir + '/',
+        '-r', args.version + '-linux',
+        '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-linux.yml',
+    ]):
+        print('Verifying v' + args.version + ' Linux FAILED\n')
         rc = 1
 
-    print('\nVerifying v'+args.version+' Windows\n')
-    if subprocess.call(['bin/gverify', '-v', '-d', '../' + GITIAN_SIGS_DIR + '/', '-r', args.version+'-win-unsigned', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win.yml']):
-        print('Verifying v'+args.version+' Windows FAILED\n')
+    print('\nVerifying v' + args.version + ' Windows\n')
+    if subprocess.call([
+        'bin/gverify', '-v', '-d', '../' + args.attestations_dir + '/',
+        '-r', args.version + '-win-unsigned',
+        '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win.yml',
+    ]):
+        print('Verifying v' + args.version + ' Windows FAILED\n')
         rc = 1
 
-    print('\nVerifying v'+args.version+' MacOS\n')
-    if subprocess.call(['bin/gverify', '-v', '-d', '../' + GITIAN_SIGS_DIR + '/', '-r', args.version+'-osx-unsigned', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx.yml']):
-        print('Verifying v'+args.version+' MacOS FAILED\n')
-        rc = 1
-
-    print('\nVerifying v'+args.version+' Signed Windows\n')
-    if subprocess.call(['bin/gverify', '-v', '-d', '../' + GITIAN_SIGS_DIR + '/', '-r', args.version+'-win-signed', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-win-signer.yml']):
-        print('Verifying v'+args.version+' Signed Windows FAILED\n')
-        rc = 1
-
-    print('\nVerifying v'+args.version+' Signed MacOS\n')
-    if subprocess.call(['bin/gverify', '-v', '-d', '../' + GITIAN_SIGS_DIR + '/', '-r', args.version+'-osx-signed', '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx-signer.yml']):
-        print('Verifying v'+args.version+' Signed MacOS FAILED\n')
+    print('\nVerifying v' + args.version + ' MacOS\n')
+    if subprocess.call([
+        'bin/gverify', '-v', '-d', '../' + args.attestations_dir + '/',
+        '-r', args.version + '-osx-unsigned',
+        '../' + SOURCE_REPO_DIR + '/contrib/gitian-descriptors/gitian-osx.yml',
+    ]):
+        print('Verifying v' + args.version + ' MacOS FAILED\n')
         rc = 1
 
     os.chdir(workdir)
     return rc
 
+
 def main():
     global args, workdir
 
-    parser = argparse.ArgumentParser(description='Script for running full Feathercoin Gitian builds.')
+    parser = argparse.ArgumentParser(description='Script for running Feathercoin unsigned Gitian builds.')
     parser.add_argument('-c', '--commit', action='store_true', dest='commit', help='Indicate that the version argument is for a commit or branch')
     parser.add_argument('-p', '--pull', action='store_true', dest='pull', help='Indicate that the version argument is the number of a github repository pull request')
     parser.add_argument('-u', '--url', dest='url', default=DEFAULT_SOURCE_URL, help='Specify the URL of the source repository. Default is %(default)s')
-    parser.add_argument('--gitian-sigs-url', dest='gitian_sigs_url', default=DEFAULT_GITIAN_SIGS_URL, help='Specify the URL of the gitian signatures repository. Default is %(default)s')
-    parser.add_argument('--detached-sigs-url', dest='detached_sigs_url', default=DEFAULT_DETACHED_SIGS_URL, help='Specify the URL of the detached signatures repository. Default is %(default)s')
+    parser.add_argument('--attestations-dir', dest='attestations_dir', default=ATTESTATIONS_DIR, help='Specify the local directory for Gitian attestation files. Default is %(default)s')
     parser.add_argument('-v', '--verify', action='store_true', dest='verify', help='Verify the Gitian build')
-    parser.add_argument('-b', '--build', action='store_true', dest='build', help='Do a Gitian build')
-    parser.add_argument('-s', '--sign', action='store_true', dest='sign', help='Make signed binaries for Windows and MacOS')
-    parser.add_argument('-B', '--buildsign', action='store_true', dest='buildsign', help='Build both signed and unsigned binaries')
+    parser.add_argument('-b', '--build', action='store_true', dest='build', help='Do an unsigned Gitian build')
     parser.add_argument('-o', '--os', dest='os', default='lwm', help='Specify which Operating Systems the build is for. Default is %(default)s. l for Linux, w for Windows, m for MacOS')
     parser.add_argument('-j', '--jobs', dest='jobs', default='2', help='Number of processes to use. Default %(default)s')
     parser.add_argument('-m', '--memory', dest='memory', default='2000', help='Memory to allocate in MiB. Default %(default)s')
     parser.add_argument('-k', '--kvm', action='store_true', dest='kvm', help='Use KVM instead of LXC')
     parser.add_argument('-d', '--docker', action='store_true', dest='docker', help='Use Docker instead of LXC')
     parser.add_argument('-S', '--setup', action='store_true', dest='setup', help='Set up the Gitian building environment. Only works on Debian-based systems (Ubuntu, Debian)')
-    parser.add_argument('-D', '--detach-sign', action='store_true', dest='detach_sign', help='Create the assert file for detached signing. Will not commit anything.')
-    parser.add_argument('-n', '--no-commit', action='store_false', dest='commit_files', help='Do not commit anything to git')
-    parser.add_argument('signer', nargs='?', help='GPG signer to sign each build assert file')
+    parser.add_argument('-D', '--detach-sign', action='store_true', dest='detach_sign', help='Create unsigned attestation files without GPG signatures')
+    parser.add_argument('--signer', dest='signer', help='GPG signer to sign each build attestation file')
     parser.add_argument('version', nargs='?', help='Version number, commit, or branch to build. If building a commit or branch, the -c option must be specified')
 
     args = parser.parse_args()
@@ -191,8 +180,6 @@ def main():
     if args.kvm and args.docker:
         raise Exception('Error: cannot have both kvm and docker')
 
-    # Ensure no more than one environment variable for gitian-builder (USE_LXC, USE_VBOX, USE_DOCKER) is set as they
-    # can interfere (e.g., USE_LXC being set shadows USE_DOCKER; for details see gitian-builder/libexec/make-clean-vm).
     os.environ['USE_LXC'] = ''
     os.environ['USE_VBOX'] = ''
     os.environ['USE_DOCKER'] = ''
@@ -208,44 +195,36 @@ def main():
     if args.setup:
         setup()
 
-    if args.buildsign:
-        args.build = True
-        args.sign = True
-
-    if not args.build and not args.sign and not args.verify:
+    if not args.build and not args.verify:
         sys.exit(0)
 
     args.linux = 'l' in args.os
     args.windows = 'w' in args.os
     args.macos = 'm' in args.os
 
-    # Disable for MacOS if no SDK found
     if args.macos and not os.path.isfile('gitian-builder/inputs/Xcode-11.3.1-11C505-extracted-SDK-with-libcxx-headers.tar.gz'):
         print('Cannot build for MacOS, SDK does not exist. Will build for other OSes')
         args.macos = False
 
-    args.sign_prog = 'true' if args.detach_sign else 'gpg --detach-sign'
-
     script_name = os.path.basename(sys.argv[0])
-    if not args.signer:
-        print(script_name+': Missing signer')
-        print('Try '+script_name+' --help for more information')
+    if args.build and not args.signer:
+        print(script_name + ': Missing signer')
+        print('Try ' + script_name + ' --help for more information')
         sys.exit(1)
     if not args.version:
-        print(script_name+': Missing version')
-        print('Try '+script_name+' --help for more information')
+        print(script_name + ': Missing version')
+        print('Try ' + script_name + ' --help for more information')
         sys.exit(1)
 
-    # Add leading 'v' for tags
     if args.commit and args.pull:
         raise Exception('Cannot have both commit and pull')
     args.commit = ('' if args.commit else 'v') + args.version
 
     os.chdir(SOURCE_REPO_DIR)
     if args.pull:
-        subprocess.check_call(['git', 'fetch', args.url, 'refs/pull/'+args.version+'/merge'])
+        subprocess.check_call(['git', 'fetch', args.url, 'refs/pull/' + args.version + '/merge'])
         os.chdir('../gitian-builder/inputs/bitcoin')
-        subprocess.check_call(['git', 'fetch', args.url, 'refs/pull/'+args.version+'/merge'])
+        subprocess.check_call(['git', 'fetch', args.url, 'refs/pull/' + args.version + '/merge'])
         args.commit = subprocess.check_output(['git', 'show', '-s', '--format=%H', 'FETCH_HEAD'], universal_newlines=True, encoding='utf8').strip()
         args.version = 'pull-' + args.version
     print(args.commit)
@@ -258,16 +237,12 @@ def main():
     os.chdir(workdir)
 
     if args.build:
+        args.sign_prog = 'true' if args.detach_sign else 'gpg --detach-sign'
         build()
 
-    if args.sign:
-        sign()
-
     if args.verify:
-        os.chdir(GITIAN_SIGS_DIR)
-        subprocess.check_call(['git', 'pull'])
-        os.chdir(workdir)
         sys.exit(verify())
+
 
 if __name__ == '__main__':
     main()
---
+++ contrib/release/create-sha256sums.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+usage() {
+    echo "Usage: $0 [release-directory]" >&2
+    exit 1
+}
+
+if [ $# -gt 1 ]; then
+    usage
+fi
+
+release_dir="${1:-.}"
+gpg_prog="${GPG:-gpg}"
+
+cd "$release_dir"
+
+rm -f SHA256SUMS SHA256SUMS.asc
+
+find . -maxdepth 1 -type f \
+    ! -name 'SHA256SUMS' \
+    ! -name 'SHA256SUMS.asc' \
+    ! -name '*.assert' \
+    ! -name '*.assert.sig' \
+    ! -name '*-debug.tar.gz' \
+    ! -name '*-debug.zip' \
+    -print0 \
+    | sort -z \
+    | xargs -r0 sha256sum > SHA256SUMS
+
+if [ ! -s SHA256SUMS ]; then
+    echo "No release artifacts found in $release_dir" >&2
+    rm -f SHA256SUMS
+    exit 1
+fi
+
+"$gpg_prog" --digest-algo sha256 --clearsign SHA256SUMS
+rm SHA256SUMS
+
+echo "Created $(pwd)/SHA256SUMS.asc"
